1e5900b997a81a85cec0b32408401d5442501284
MuhammadRifki12 <muhammadrifki120403@gmail.com>
2023-12-02 10:20:04 +0800

    Pertama

---
+++ PenjualanSepatuSendal.dpr
@@ -0,0 +1,13 @@
+program PenjualanSepatuSendal;
+
+uses
+  Forms,
+  Flogin in 'Flogin.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
